b714f9255de1050098639837c90fa1603c15cbe4
viktordulo <victordulo96@gmail.com>
2021-01-17 22:13:33 +0200

    added ProjectList class

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,3 +1,42 @@
+class ProjectList {
+
+    /** The template element which we want to display. */
+    templateElement: HTMLTemplateElement;
+
+
+    /** The element which appends the template. */
+    hostElement: HTMLDivElement;
+
+
+    /** Element which is inserted into @see hostElement */
+    element: HTMLElement;
+
+    constructor(private type: 'active' | 'finished') {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-list');
+        this.hostElement = <HTMLDivElement>document.getElementById('app');
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = <HTMLElement>importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+
+        this.attach();
+        this.renderContent();
+    }
+
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul')!.id = listId;
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+    }
+
+
+    /** Displays the template. */
+    private attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
+
 class ProjectInput {
 
     /** The template element which we want to display. */
@@ -103,6 +142,8 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
 
 
 //#region Validation
